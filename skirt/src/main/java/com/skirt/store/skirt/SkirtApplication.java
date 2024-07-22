043218452f8d36648470370cc71847fdd75b8182
Carlos Henrique <ichfs9108@gmail.com>
2024-07-22 15:49:01 -0300

    project created

---
+++ skirt/src/main/java/com/skirt/store/skirt/SkirtApplication.java
@@ -0,0 +1,13 @@
+package com.skirt.store.skirt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkirtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkirtApplication.class, args);
+	}
+
+}
---
+++ skirt/src/test/java/com/skirt/store/skirt/SkirtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skirt.store.skirt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkirtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
