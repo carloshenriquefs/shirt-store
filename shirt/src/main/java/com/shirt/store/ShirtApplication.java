da7f3f43d36294dffdeeb8ffce16ebd9d153d132
Carlos Henrique <ichfs9108@gmail.com>
2024-07-23 23:13:26 -0300

    fix: Ajustando estrutura do projeto

--- shirt/src/main/java/com/shirt/store/ShirtApplication.java
+++ shirt/src/main/java/com/shirt/store/ShirtApplication.java
@@ -1,13 +1,13 @@
-package com.skirt.store.skirt;
+package com.shirt.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SkirtApplication {
+public class ShirtApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SkirtApplication.class, args);
+		SpringApplication.run(ShirtApplication.class, args);
 	}
 
 }
--- shirt/src/test/java/com/shirt/store/ShirtApplicationTests.java
+++ shirt/src/test/java/com/shirt/store/ShirtApplicationTests.java
@@ -1,10 +1,10 @@
-package com.skirt.store.skirt;
+package com.shirt.store;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SkirtApplicationTests {
+class ShirtApplicationTests {
 
 	@Test
 	void contextLoads() {
